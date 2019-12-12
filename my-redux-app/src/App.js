765ba9055b94b981e6cd8c0224109873b1e4e96a
Javier <tsakasu88@gmail.com>
2019-12-12 17:02:16 -0500

    made a react app using redux , react-redux

---
+++ my-redux-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import UserData from './components/UserData'
+
+function App() {
+  return (
+    <div className="Kanye">
+      <UserData/>
+    </div>
+  )
+}
+
+export default App;
